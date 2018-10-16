f8369f1c18df4e62a2d3c3eac291ad1de4fc0fa9
LuidjyDev <44212583+LuidjyDev@users.noreply.github.com>
2018-10-16 22:51:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,15 +1,11 @@
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+var bot = new Discord.Client();
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+bot.on("ready", function() {
+  bot.user.setGame("CLDEV-Bot, !CLDev");
+ console.log("Le Bot s'est bien connecté");
 });
  
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
  
-client.login('XsuBq6X6AicCsqXtn7qqli4LsQUCxp0N');
+bot.login('XsuBq6X6AicCsqXtn7qqli4LsQUCxp0N');
